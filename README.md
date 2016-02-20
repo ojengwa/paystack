48ba256bc61649202f0bfa39d4d65ea347f39430
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-20 22:22:55 +0100

    base project setup

---
+++ README.md
@@ -0,0 +1,33 @@
+paystack
+===============================
+
+version number: 0.0.1
+author: Bernard Ojengwa
+
+Overview
+--------
+
+A Paystack API wrapper in Python
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install paystack
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/ojengwa/paystack.git
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+VERSION = '0.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '')
+                    for x in all_reqs if 'git+' not in x]
+
+setup(
+    name='paystack',
+    version=VERSION,
+    description='A Paystack API wrapper in Python',
+    long_description=long_description,
+    url='https://github.com/ojengwa/paystack',
+    download_url='https://github.com/ojengwa/paystack/tarball/' + VERSION,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Bernard Ojengwa',
+    install_requires=install_requires,
+    depedency_links=dependency_links,
+    author_email='bernardojengwa@gmail.com'
+)
