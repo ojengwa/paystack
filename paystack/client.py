df1cec546fab80ab00bc64481fc967816b6ab1ca
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-21 15:36:10 +0100

    create request classes

--- paystack/client.py
+++ paystack/client.py
@@ -32,23 +32,122 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-
+import os
 import requests
 
 
-class PaystackSDK(object):
-    """docstring for PaystackSDK
+class HTTPClient(object):
 
-    Attributes:
-        arg (TYPE): Description
-    """
+    def __init__(self, verify_ssl_certs=True):
+        self._verify_ssl_certs = verify_ssl_certs
 
-    def __init__(self, arg):
-        """
-        Summary.
+    def request(self, method, url, headers, post_data=None):
+        raise NotImplementedError(
+            'HTTPClient subclasses must implement `request`')
 
-        Args:
-            arg (TYPE): Description
-        """
-        super(PaystackSDK, self).__init__()
-        self.arg = arg
+
+class RequestsClient(HTTPClient):
+    name = 'requests'
+
+    def request(self, method, url, headers, post_data=None):
+        kwargs = {}
+
+        if self._verify_ssl_certs:
+            kwargs['verify'] = os.path.join(
+                os.path.dirname(__file__), 'data/ca-certificates.crt')
+        else:
+            kwargs['verify'] = False
+
+        try:
+            try:
+                result = requests.request(method,
+                                          url,
+                                          headers=headers,
+                                          data=post_data,
+                                          timeout=80,
+                                          **kwargs)
+            except TypeError as e:
+                raise TypeError(
+                    'Warning: It looks like your installed version of the '
+                    '"requests" library is not compatible with Paystack\'s '
+                    'usage thereof. (HINT: The most likely cause is that '
+                    'your "requests" library is out of date. You can fix '
+                    'that by running "pip install -U requests".) The '
+                    'underlying error was: %s' % (e,))
+
+            # This causes the content to actually be read, which could cause
+            # e.g. a socket timeout. TODO: The other fetch methods probably
+            # are susceptible to the same and should be updated.
+            content = result.content
+            status_code = result.status_code
+        except Exception as e:
+            # Would catch just requests.exceptions.RequestException, but can
+            # also raise ValueError, RuntimeError, etc.
+            self._handle_request_error(e)
+        return content, status_code, result.headers
+
+    def _handle_request_error(self, e):
+        if isinstance(e, requests.exceptions.RequestException):
+            msg = ("Unexpected error communicating with Stripe.  "
+                   "If this problem persists, let us know at "
+                   "support@stripe.com.")
+            err = "%s: %s" % (type(e).__name__, str(e))
+        else:
+            msg = ("Unexpected error communicating with Stripe. "
+                   "It looks like there's probably a configuration "
+                   "issue locally.  If this problem persists, let us "
+                   "know at support@stripe.com.")
+            err = "A %s was raised" % (type(e).__name__,)
+            if str(e):
+                err += " with error message %s" % (str(e),)
+            else:
+                err += " with no error message"
+        msg = textwrap.fill(msg) + "\n\n(Network error: %s)" % (err,)
+        raise error.APIConnectionError(msg)
+
+
+class UrlFetchClient(HTTPClient):
+    name = 'urlfetch'
+
+    def __init__(self, verify_ssl_certs=True, deadline=55):
+        self._verify_ssl_certs = verify_ssl_certs
+        # GAE requests time out after 60 seconds, so make sure to default
+        # to 55 seconds to allow for a slow Stripe
+        self._deadline = deadline
+
+    def request(self, method, url, headers, post_data=None):
+        try:
+            result = urlfetch.fetch(
+                url=url,
+                method=method,
+                headers=headers,
+                # Google App Engine doesn't let us specify our own cert bundle.
+                # However, that's ok because the CA bundle they use recognizes
+                # api.stripe.com.
+                validate_certificate=self._verify_ssl_certs,
+                deadline=self._deadline,
+                payload=post_data
+            )
+        except urlfetch.Error as e:
+            self._handle_request_error(e, url)
+
+        return result.content, result.status_code, result.headers
+
+    def _handle_request_error(self, e, url):
+        if isinstance(e, urlfetch.InvalidURLError):
+            msg = ("The Stripe library attempted to fetch an "
+                   "invalid URL (%r). This is likely due to a bug "
+                   "in the Stripe Python bindings. Please let us know "
+                   "at support@stripe.com." % (url,))
+        elif isinstance(e, urlfetch.DownloadError):
+            msg = "There was a problem retrieving data from Stripe."
+        elif isinstance(e, urlfetch.ResponseTooLargeError):
+            msg = ("There was a problem receiving all of your data from "
+                   "Stripe.  This is likely due to a bug in Stripe. "
+                   "Please let us know at support@stripe.com.")
+        else:
+            msg = ("Unexpected error communicating with Stripe. If this "
+                   "problem persists, let us know at support@stripe.com.")
+
+        msg = textwrap.fill(msg) + "\n\n(Network error: " + str(e) + ")"
+        raise error.APIConnectionError(msg)
